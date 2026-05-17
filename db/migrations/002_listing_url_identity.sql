27dd1a5204290e3459087ab72b15354cfec5ef6b
profishional <profishcrypto@gmail.com>
2026-05-17 12:45:36 +0100

    Reset repository contents

--- db/migrations/002_listing_url_identity.sql
+++
@@ -1,4 +0,0 @@
-DROP INDEX IF EXISTS marketplace_listings_source_url_observed_at_idx;
-
-CREATE UNIQUE INDEX IF NOT EXISTS marketplace_listings_source_url_observed_at_title_idx
-  ON marketplace_listings (source, url, observed_at, title);
